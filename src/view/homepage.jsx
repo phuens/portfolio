76da26e985fa61522766710ff90fec914bf8d489
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2022-08-19 23:08:56 -0600

    test

--- src/view/homepage.jsx
+++ src/view/homepage.jsx
@@ -19,6 +19,7 @@ export default function Homepage() {
                 const response = await fetch('https://notion-books-api.herokuapp.com');
                 const result = await response.json();
                 setBooks(result);
+                console.log('this is the result');
                 setLoading(false);
             } catch (e) {
                 console.error(e);
