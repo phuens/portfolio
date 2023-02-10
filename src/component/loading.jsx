dded1126db7a81b6ed560c1e11c69cd539e310e3
Phuens <norbu_phuntsho@wheatoncollege.edu>
2023-02-09 19:08:59 -0600

    FEAT: loading screen on data load for TIL

--- src/component/loading.jsx
+++ src/component/loading.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
-import Modal from '@mui/material/Modal';
-import { BsFillArrowRightCircleFill } from 'react-icons/bs';
-
 import { FACTS } from '../constant/facts';
 import LoaderGIF from '../asset/images/loader/loader.gif';
 
-export default function Loader({ text, books }) {
+export default function Loader({text}) {
     const [index, setIndex] = useState(0);
-    const [modalOpen, setModalOpen] = useState(false);
 
     const randomNumber = (max) => {
         return Math.floor(Math.random() * max + 1);
@@ -24,10 +20,10 @@ export default function Loader({ text, books }) {
 
     return (
         <div className="flex flex-col items-center">
-            <img src={LoaderGIF} alt="loading gif" className="w-1/12" />
-            <div className="text-md text-gray-400 mb-6 md:text-md">{text}</div>
+            <p className='-mb-8 text-2xl'> {text} </p>
+            <img src={LoaderGIF} alt="loading gif" className="w-2/12"/>
 
-            <div className="mt-4 h-screen w-full p-2 text-center md:p-6 md:w-8/12">
+            <div className="mt-4 h-screen w-full p-2 text-center md:p-2 md:w-8/12">
                 <>
                     <p className="text-xl font-bold text-blue-400 md:text-2xl">Did you know</p>
                     <div className="text-3xl md:text-4xl my-6">
@@ -41,67 +37,6 @@ export default function Loader({ text, books }) {
                         </a>
                     </p>
                 </>
-
-                {books ? (
-                    <div>
-                        <button
-                            className="w-full flex justify-center"
-                            onClick={() => setModalOpen(true)}
-                        >
-                            <div className="mt-36 text-sm flex py-2 justify-center font-thin w-full md:w-12/12 ">
-                                Why is this taking too long
-                                <span className="mx-2 pt-1">
-                                    <BsFillArrowRightCircleFill className="" />
-                                </span>
-                            </div>
-                        </button>
-                        <Modal
-                            open={modalOpen}
-                            onClose={() => setModalOpen(false)}
-                            aria-labelledby="modal-modal-title"
-                            aria-describedby="modal-modal-description"
-                        >
-                            <div className="w-12/12 mx-3 mt-24 flex justify-center">
-                                <div
-                                    className="px-4 py-12 md:w-6/12 md:p-12"
-                                    style={{
-                                        backgroundColor: '#070911',
-                                        boxShadow: '1px 2px 12px -1px rgba(255,255,255,0.66)',
-                                    }}
-                                >
-                                    <p className="text-md text-center mb-4 text-white md:text-2xl">
-                                        THANK YOU FOR WAITING ✌️
-                                        <button
-                                            onClick={() => setModalOpen(false)}
-                                            className="border px-4 rounded-xl text-sm text-white float-right md:mt-2"
-                                        >
-                                            Close
-                                        </button>
-                                    </p>
-
-                                    <hr className="border-b border-gray-800 mb-4" />
-
-                                    <p className="mt-4 text-white leading-loose">
-                                        The book information is being{' '}
-                                        <b>fetched from a notion database </b>
-                                        that I use to update my reading list. In the name of
-                                        automation, I fetch the data directly from notion so that
-                                        the website will automatically update what I have read and
-                                        am currently reading. However, my static website without a
-                                        server cannot directly fetch data from the notion API. So I
-                                        built a python Heroku app that acts as a{' '}
-                                        <b>proxy to fetch the data from notion API</b> and send it
-                                        to my website. This is unfortunately really slow but
-                                        <b> thank you </b>for reading this and waiting for the books
-                                        to load.
-                                    </p>
-                                </div>
-                            </div>
-                        </Modal>
-                    </div>
-                ) : (
-                    ''
-                )}
             </div>
         </div>
     );
--- src/view/TIL.jsx
+++ src/view/TIL.jsx
@@ -4,6 +4,7 @@ import {collection, doc, getDocs, setDoc, Timestamp, updateDoc} from 'firebase/f
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import moment from 'moment'
 
+import Loader from '../component/loading'
 import db from '../api/firestore'
 
 export default function TIL() {
@@ -47,7 +48,6 @@ export default function TIL() {
 
     const submitData = async (data) => {
         const time = Timestamp.fromDate(new Date( moment(data.date).toDate()))
-        console.log("===>", data)
         await setDoc(doc(db, 'TIL/' + time), {
             content: data.content, 
             category: data.category, 
@@ -119,7 +119,7 @@ export default function TIL() {
                     <button onClick={() => handleLike(data.id, data.liked)}> 🫰 {data.liked}</button>
                     <p>{data.date}</p>
                     <div className="">
-                        <div className='text-xs text-gray-400'>Source: {data.source ? <a href={data.link} target="_" className='text-blue-400' aria-disabled={data.link === '#'} role="link" >{data.source}</a> : <i className='text-gray-200'>NA</i>}</div>                    
+                        <div className='text-xs text-gray-400'>Source: {data.source ? <a href={data.link} target="_" className='text-blue-400' aria-disabled={data.link === '#'}>{data.source}</a> : <i className='text-gray-200'>NA</i>}</div>                    
                     </div>
                 </div>
                 
@@ -130,105 +130,111 @@ export default function TIL() {
     return (
         <>
         <div className="flex flex-col ">
-            <div className='flex flex-row'>
-                <div className='w-10/12'>
-                    <span style={{ fontFamily: 'Major Mono Display' }}>
-                        <b>today i learned</b>
-                    </span>
-                </div>
-                
+            {!items.length ? ( 
+                <div>
+                    <Loader text="Loading"/>
+                </div>): 
+            (
+                <div className='flex flex-row'>
+                    <div className='w-10/12'>
+                        <span style={{ fontFamily: 'Major Mono Display' }}>
+                            <b>today i learned</b>
+                        </span>
+                    </div>
+                    
 
-                <div className="w-full float-right">
-                    <button
-                        className="border flex flex-row px-4 md:px-4 rounded-xl bg-white text-sm text-blue-900 float-right"
-                        onClick={() => setModalOpen(true)}
-                    >
-                       Upload 🤔
-                    </button>
-                    <Modal
-                        open={modalOpen}
-                        onClose={() => setModalOpen(false)}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                    >
-                        <div className="w-12/12 mt-8 md:mt-24 flex justify-center ">
-                            <div
-                                className="px-4 py-8 md:py-12 w-11/12 md:w-10/12 md:p-12 rounded-xl"
-                                style={{
-                                    backgroundColor: '#070911',
-                                    boxShadow: '1px 2px 12px -1px rgba(255,255,255,0.66)',
-                                }}
-                            >
-                                <p className="text-md text-center mb-16 md:mb-20 text-white md:text-2xl">
-                                   
-                                    <button
-                                        onClick={() => {setModalOpen(false); setErrors({})}}
-                                        style={{
-                                            boxShadow:  "1px 1px 1px #000, -1px -1px 3px #090c16"
-                                        }}
-                                        className="px-4 py-3 transition-all text-sm text-white float-right md:mt-2 hover:text-indigo-900 hover:rounded-full"
-                                    >
-                                        X
-                                    </button>
-                                </p>
-                                <form className="flex flex-col px-4">
+                    <div className="w-full float-right">
+                        <button
+                            className="border flex flex-row px-4 md:px-4 rounded-xl bg-white text-sm text-blue-900 float-right"
+                            onClick={() => setModalOpen(true)}
+                        >
+                        Upload 🤔
+                        </button>
+                        <Modal
+                            open={modalOpen}
+                            onClose={() => setModalOpen(false)}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <div className="w-12/12 mt-8 md:mt-24 flex justify-center ">
+                                <div
+                                    className="px-4 py-8 md:py-12 w-11/12 md:w-10/12 md:p-12 rounded-xl"
+                                    style={{
+                                        backgroundColor: '#070911',
+                                        boxShadow: '1px 2px 12px -1px rgba(255,255,255,0.66)',
+                                    }}
+                                >
+                                    <p className="text-md text-center mb-16 md:mb-20 text-white md:text-2xl">
+                                    
+                                        <button
+                                            onClick={() => {setModalOpen(false); setErrors({})}}
+                                            style={{
+                                                boxShadow:  "1px 1px 1px #000, -1px -1px 3px #090c16"
+                                            }}
+                                            className="px-4 py-3 transition-all text-sm text-white float-right md:mt-2 hover:text-indigo-900 hover:rounded-full"
+                                        >
+                                            X
+                                        </button>
+                                    </p>
+                                    <form className="flex flex-col px-4">
 
-                                    <div className='flex flex-row flex-wrap md:justify-between my-2'>
-                                        <div className="w-full md:w-6/12 flex flex-col">
-                                            <label className='text-white my-2'>
-                                                <input onChange={handleChange} className="p-2 text-black w-full rounded-sm" name="category" type="text" placeholder="Category 📂"/>  
-                                            </label>
-                                            {errors.category && <p className="text-red-500">{errors.category}</p>}
-                                        </div>
-                                        <div className=" w-full md:w-6/12 flex flex-col">
-                                            <label className='text-white my-2'>
-                                                <input onChange={handleChange} className="p-2 float-right text-black w-full md:w-11/12 rounded-sm" name="date" type="date"/>  
-                                            </label>
-                                            {errors.date && <p className="text-red-500 text-right">{errors.date}</p>}
-                                        </div>
-                                        
-                                    </div>
-                                    <div className='flex flex-col mb-4'>
-                                        <label className='text-white my-2'>
-                                            <textarea onChange={handleChange} className="p-2 text-black w-full rounded-sm" rows="10" name="content" type="textarea" placeholder="Content 📖"/>  
-                                        </label>
-                                        {errors.content && <p className="text-red-500">{errors.content}</p>}
-                                        <label className='text-white my-2'>
-                                            <input onChange={handleChange} className="p-2 text-black w-full rounded-sm" name="source" type="text" placeholder="Source 🧬"/>  
-                                        </label>
-                                        <label className='text-white my-2'>
-                                            <input onChange={handleChange} className="p-2 text-black w-full rounded-sm" name="link" type="text" placeholder="Source Link 🔗"/>  
-                                        </label>
-                                    </div>
-                                    <div className="flex flex-row flex-wrap w-full">0
-                                        <div className="flex flex-wrap w-full md:w-12/12">
-                                            <label className='text-white my-2 w-full md:w-6/12'>
-                                                <input onChange={handleChange} className="p-2 text-black w-full md:w-10/12 rounded-sm" name="email" type="email" placeholder="email 📪"/>  
-                                                {errors.email && <p className="text-red-500">{errors.email}</p>}
-                                            </label>
+                                        <div className='flex flex-row flex-wrap md:justify-between my-2'>
+                                            <div className="w-full md:w-6/12 flex flex-col">
+                                                <label className='text-white my-2'>
+                                                    <input onChange={handleChange} className="p-2 text-black w-full rounded-sm" name="category" type="text" placeholder="Category 📂"/>  
+                                                </label>
+                                                {errors.category && <p className="text-red-500">{errors.category}</p>}
+                                            </div>
+                                            <div className=" w-full md:w-6/12 flex flex-col">
+                                                <label className='text-white my-2'>
+                                                    <input onChange={handleChange} className="p-2 float-right text-black w-full md:w-11/12 rounded-sm" name="date" type="date"/>  
+                                                </label>
+                                                {errors.date && <p className="text-red-500 text-right">{errors.date}</p>}
+                                            </div>
                                             
-
-                                            <label className='text-white my-2 w-full md:w-4/12'>
-                                                <input onChange={handleChange} className="p-2 text-black w-full md:w-10/12 rounded-sm" name="password" type="password" placeholder="Password 🔑"/>  
-                                                {errors.password && <p className="text-red-500">{errors.password}</p>}
+                                        </div>
+                                        <div className='flex flex-col mb-4'>
+                                            <label className='text-white my-2'>
+                                                <textarea onChange={handleChange} className="p-2 text-black w-full rounded-sm" rows="10" name="content" type="textarea" placeholder="Content 📖"/>  
                                             </label>
-                                           
+                                            {errors.content && <p className="text-red-500">{errors.content}</p>}
+                                            <label className='text-white my-2'>
+                                                <input onChange={handleChange} className="p-2 text-black w-full rounded-sm" name="source" type="text" placeholder="Source 🧬"/>  
+                                            </label>
+                                            <label className='text-white my-2'>
+                                                <input onChange={handleChange} className="p-2 text-black w-full rounded-sm" name="link" type="text" placeholder="Source Link 🔗"/>  
+                                            </label>
+                                        </div>
+                                        <div className="flex flex-row flex-wrap w-full">0
+                                            <div className="flex flex-wrap w-full md:w-12/12">
+                                                <label className='text-white my-2 w-full md:w-6/12'>
+                                                    <input onChange={handleChange} className="p-2 text-black w-full md:w-10/12 rounded-sm" name="email" type="email" placeholder="email 📪"/>  
+                                                    {errors.email && <p className="text-red-500">{errors.email}</p>}
+                                                </label>
+                                                
+
+                                                <label className='text-white my-2 w-full md:w-4/12'>
+                                                    <input onChange={handleChange} className="p-2 text-black w-full md:w-10/12 rounded-sm" name="password" type="password" placeholder="Password 🔑"/>  
+                                                    {errors.password && <p className="text-red-500">{errors.password}</p>}
+                                                </label>
                                             
-                                            <div className='py-4 w-full text-center md:w-1/12 md:text-left'>
-                                                <button onClick={(e) => handleSubmit(e)} className="border px-12 md:px-4 rounded-xl text-md text-white hover:rounded-md hover:px-6 transition-all">
-                                                    Submit
-                                                </button>
+                                                
+                                                <div className='py-4 w-full text-center md:w-1/12 md:text-left'>
+                                                    <button onClick={(e) => handleSubmit(e)} className="border px-12 md:px-4 rounded-xl text-md text-white hover:rounded-md hover:px-6 transition-all">
+                                                        Submit
+                                                    </button>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="mt-2 text-red-500"> {authError && <p>Authentication failed. {authMsg}</p>}</div>                              
-                                </form>
+                                        <div className="mt-2 text-red-500"> {authError && <p>Authentication failed. {authMsg}</p>}</div>                              
+                                    </form>
 
+                                </div>
                             </div>
-                        </div>
-                    </Modal>
+                        </Modal>
+                    </div>
                 </div>
-            </div>
+            )}
     
             <hr className='mt-2 mb-8'/>
             
